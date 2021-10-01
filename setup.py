71cc1cade143e0efd9245bc5ff8c2096eff128c5
Taiwo Sumayyah <31887313+Sumta4real@users.noreply.github.com>
2021-10-02 00:41:09 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup, find_packages
+HERE = pathlib.Path(__file__).parent
+VERSION = '0.0.1'
+PACKAGE_NAME = 'EDA'
+AUTHOR = 'Sumayyah Taiwo'
+AUTHOR_EMAIL = 'sumty4deen@.com'
+URL = 'https://github.com/Sumta4real/ExploratoryDataAnalysis'
+LICENSE = 'MIT'
+DESCRIPTION = 'A package that performs Exploratory Data Analysis'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas'
+      'matplotlib'
+      'seaborn'
+      'scipy'
+]
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
